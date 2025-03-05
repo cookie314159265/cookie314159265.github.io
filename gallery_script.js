5e084b9d1efc7412d15e4223d125672ac74a2b04
cookie314159265 <nataliekyon@gmail.com>
2025-03-04 16:20:49 -0800

    Create gallery_script.js

---
+++ gallery_script.js
@@ -0,0 +1,5 @@
+const imageGallery = [
+    "https://images.pexels.com/photos/2382868/pexels-photo-2382868.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/2416602/pexels-photo-2416602.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/2782485/pexels-photo-2782485.jpeg?auto=compress&cs=tinysrgb&w=1200"
+]
